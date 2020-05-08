9aa26e95dba55a8be1c7e17b8416a977bd50d31d
Annalise Murphy <nali247@gmail.com>
2020-05-07 22:36:14 -0700

    added pages for front-end

---
+++ AboutPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AboutPage extends Component {
+    render() {
+        return (
+            <div>
+                This section is about me. More to come!
+            </div>
+        )
+    }
+}
---
+++ Herbs.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import request from 'superagent';
+import List from './List.js';
+
+export default class herbs extends Component {
+    state = {
+        loading: true
+    }
+    async componentDidMount() {
+        const data = await request.get('https://still-plateau-72252.herokuapp.com/herbs');
+        this.setState({mushrooms: data.body, loading: false})
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.state.loading ?
+                    <h2>Loading</h2>
+                    :
+                    <List data={this.state.herbs}/>
+                }
+            </div>
+        )
+    }
+}
